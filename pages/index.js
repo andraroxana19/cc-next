044a0e37a38f575b12ca7658fa86694afe895896
andraroxana19 <enacheandra19@stud.ase.ro>
2023-05-15 13:16:11 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import MainPage from "@/js/components/MainPage";
+import Image from 'next/image'
+import MyImage from "@/public/pizza1.jpg";
+
+export default function Home() {
+  return (
+      <MainPage>
+
+
+      </MainPage>
+  )
+}
